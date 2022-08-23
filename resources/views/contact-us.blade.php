dd47f5745afeba62094e58407f0832c157ce4fdd
Cod-kumar <1606.nk@gmail.com>
2022-08-24 00:03:26 +0530

    google

--- resources/views/contact-us.blade.php
+++ resources/views/contact-us.blade.php
@@ -27,7 +27,7 @@
 
       @if ($errors->has('g-recaptcha-response'))
           <span class="help-block">
-              <strong>{{ $errors->first('g-recaptcha-response') }}</strong>
+              <strong class="alert alert-danger">{{ $errors->first('g-recaptcha-response') }}</strong>
           </span>
       @endif
       
