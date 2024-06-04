3200d0758fec47813c7fdfbed896bb7c9d77f4e3
4606-raj <4606.raj@gmail.com>
2024-06-05 00:48:45 +0530

    footer-fix

--- resources/views/admin/layouts/footer.blade.php
+++ resources/views/admin/layouts/footer.blade.php
@@ -3,7 +3,7 @@
     <div class="container">
         <div class="row align-items-center flex-row-reverse">
             <div class="col-md-12 col-sm-12 mt-3 mt-lg-0 text-center">
-                Copyright © 2021 <a href="#">Dayone</a>. Designed by <a href="#">Spruko Technologies Pvt.Ltd</a> All rights reserved.
+                Copyright © 2024. Designed by <a href="https://stage.theplayerscorner.co.uk">The Players Corner</a> All rights reserved.
             </div>
         </div>
     </div>
