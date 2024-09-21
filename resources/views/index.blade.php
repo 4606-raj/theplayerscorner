0e56b472c77bd187ce1038682909e7816448b781
Cod-kumar <1606.nk@gmail.com>
2024-09-22 02:31:31 +0530

    Reviews-added

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -274,8 +274,10 @@
     
             </div>
         </div> --}}
-    
+        <div class="embedsocial-hashtag" data-ref="974cdf8c579edd92c8514161cca1f15accdfee36"></div> <script> (function(d, s, id) { var js; if (d.getElementById(id)) {return;} js = d.createElement(s); js.id = id; js.src = "https://embedsocial.com/cdn/ht.js"; d.getElementsByTagName("head")[0].appendChild(js); }(document, "script", "EmbedSocialHashtagScript")); </script>
 </section>
+
+
 <!-- Social Media Section END -->
 
 {{-- Contact Us START --}}
